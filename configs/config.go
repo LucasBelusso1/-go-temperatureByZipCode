3016c7ddcb7523054566c369ec7f07187532bc62
LucasBelusso1 <belussolucas@gmail.com>
2024-03-04 09:46:41 -0300

    feat: change loadconfig to load from OS environment

--- configs/config.go
+++ configs/config.go
@@ -12,44 +12,15 @@ type Config struct {
 }
 
 func LoadConfig() {
-	err := LoadConfigFromOs()
-	if err == nil {
-		return
-	}
-
-	log.Println(err.Error())
-
-	viper.SetConfigFile(".env")
-	err = viper.ReadInConfig()
-
-	if err != nil {
-		panic(err.Error())
-	}
-
-	viper.AutomaticEnv()
-
-	err = viper.Unmarshal(&config)
-	if err != nil {
-		log.Fatalf("Unable to decode into struct, %v", err)
-	}
-
-	log.Printf("Using config %+v", config)
-	log.Printf("Using port %s", config.Port)
-	log.Printf("Using weather api key %s", config.WeatherApiKey)
-}
-
-func LoadConfigFromOs() error {
 	config.WeatherApiKey = os.Getenv("WEATHER_API_KEY")
 	if config.WeatherApiKey == "" {
-		return errors.New("WEATHER_API_KEY must be set")
+		panic("WEATHER_API_KEY must be set")
 	}
 
 	config.Port = os.Getenv("PORT")
 	if config.Port == "" {
-		return errors.New("PORT must be set")
+		panic("PORT must be set")
 	}
-
-	return nil
 }
 
 func GetConfig() Config {
